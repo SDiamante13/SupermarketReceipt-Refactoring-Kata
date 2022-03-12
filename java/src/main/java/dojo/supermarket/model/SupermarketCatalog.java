9222ad685566d2f2dd743a8b2db6dbc761855fba
Steven Diamante <stevendiamante13@gmail.com>
2022-03-12 16:35:52 -0500

    Get rid of useless total price method in Catalog.

--- java/src/main/java/dojo/supermarket/model/SupermarketCatalog.java
+++ java/src/main/java/dojo/supermarket/model/SupermarketCatalog.java
@@ -3,5 +3,4 @@ package dojo.supermarket.model;
 public interface SupermarketCatalog {
     void addProduct(Product product, double price);
     double getUnitPrice(Product product);
-    double getTotalPrice(Product product, double quantity);
 }
